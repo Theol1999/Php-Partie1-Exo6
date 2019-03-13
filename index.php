49bc01680e311a926d27c19adb1b733c8735db11
theoL <theo1007@outlook.fr>
2019-03-13 15:13:54 +0100

    Ajout de l'exo 6

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$name = Theo
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title></title>
+  </head>
+  <body>
+    <p>
+      <?php
+        echo "Bonjour $name , Comment vas-tu ?"
+      ?>
+    </p>
+  </body>
+</html>
